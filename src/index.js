ea735f0c8aa0991e61456d5a3ee91e5661c61dee
Ann Sarviro <annka_nuha@ai.ua>
2022-11-23 13:35:50 +0200

    up

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './copmponents/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="Aleannlabtest">
       <App />
     </BrowserRouter>
   </React.StrictMode>
